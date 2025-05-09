3f997227d1d8678712eefe44658b2f07387a545b
muhammad abdullah <abdullahkhosa11223344@gmail.com>
2025-05-09 23:55:03 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+person={
+    'city':'dera ghazi khan',
+    'profassion':'programer',
+    'name':'m.abdullah'
+}
+#display 
+print('personal detail:',person)
+#display 
+print('Name:',person['name'])
+#add a key value 
+person['age ']=25
+#updated 
+print('updated personal detail:',person)
+a={'n':'a','b':'c'}
+print(a)
